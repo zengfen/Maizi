0dc494468677dbfad552ec752013b49eb5d3fb25
zengfen <zengfen@xjgreat.com>
2018-08-16 14:41:50 +0800

    feature1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
     def index
         @users = User.all
+        'feature1'
     end
     def download
         filename = Campus::Schoolwork::HomeModel.download_news_detail(params[:tab])
